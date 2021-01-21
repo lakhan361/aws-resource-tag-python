422b181ee474e1446453db1db35089e6bde0dfa4
Lakhan Kawre <kawre36@gmail.com>
2021-01-22 03:11:51 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,29 +2,20 @@ import boto3
 import argparse
 import csv
 
+#https://cevo.com.au/post/2018-05-10-aws-resource-tagging-api/
 
-
-output_file="lakhan.csv"
+output_file="Resource-with-tag.csv"
+Tag_key="Name"
+Tag_value="kslave2"
+filter=[{'Key': Tag_key,'Values': [Tag_value]}]
 
 
 restag = boto3.client("resourcegroupstaggingapi")
-response = restag.get_resources(ResourcesPerPage=50)
+response = restag.get_resources(TagFilters=filter,ResourcesPerPage=50)
 tag_set=[]
 for arn in response["ResourceTagMappingList"]:
-    t=(arn["ResourceARN"])
-#    print(arn["Tags"])
 
 
-list12=(t.strip().split(':', 6))
-
-
-list12=(list(set(list12)))
-for i in (list12):
-    if "/" in i:
-        print(i.strip().split('/', 1))
-
-
-"""
     for tag in arn["Tags"]:
         if tag.get('Key'):
             t=tag.get('Key')
@@ -32,18 +23,30 @@ for i in (list12):
 tag_set = list(set(tag_set))
 print(tag_set)
 
+
 with open(output_file, 'w') as csvfile:
-    fieldnames = ['ResourceARN'] + tag_set
+    fieldnames = ['ResourceARN'] +['Resource_type']+['Resource_id']+ tag_set
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
     restag = boto3.client("resourcegroupstaggingapi")
-    response = restag.get_resources(ResourcesPerPage=50)
+    response = restag.get_resources(TagFilters=filter,ResourcesPerPage=50)
     for arn in response["ResourceTagMappingList"]:
+        t=(arn["ResourceARN"])
+        list12=(t.strip().split(':', 6))
+        list12=(list(set(list12)))
+        for i in (list12):
+            if "/" in i:
+                type_id=(i.strip().split('/', 1))
+                type=type_id[0]
+                id=type_id[1]
+
         row = {}
         for tag in arn["Tags"]:
             if tag.get('Value'):
                 row[tag.get('Key')] = tag.get('Value')
         row['ResourceARN'] = arn["ResourceARN"]
+        row['Resource_type']=type
+        row['Resource_id']=id
+
         writer.writerow(row)
-"""
